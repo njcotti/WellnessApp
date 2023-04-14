7ee3d820e5f10365b9961d8dc42d0f1ac36cf391
ncotti2020 <ncotti2020@mt.fit.edu>
2023-04-14 18:49:37 -0400

    simple implementation added to each class

--- Team1Iteration/src/Health.java
+++ Team1Iteration/src/Health.java
@@ -1,64 +1,80 @@
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class Health {
-    private List<String> medicationList;
-    private Map<String, Integer> medicationCountMap;
 
-    public Health() {
-        this.medicationList = new ArrayList<>();
-        this.medicationCountMap = new HashMap<>();
-    }
-
-    public void addMedication(String medicationName) {
-        medicationList.add(medicationName);
-        medicationCountMap.put(medicationName, 0);
-    }
-
-    public void removeMedication(String medicationName) {
-        medicationList.remove(medicationName);
-        medicationCountMap.remove(medicationName);
-    }
-
-    public int getMedicationCount(String medicationName) {
-        if (medicationCountMap.containsKey(medicationName)) {
-            return medicationCountMap.get(medicationName);
-        }
-        return 0;
-    }
-
-    public void incrementMedicationCount(String medicationName) {
-        if (medicationCountMap.containsKey(medicationName)) {
-            int count = medicationCountMap.get(medicationName);
-            medicationCountMap.put(medicationName, count + 1);
-        }
-    }
-
-    public void decrementMedicationCount(String medicationName) {
-        if (medicationCountMap.containsKey(medicationName)) {
-            int count = medicationCountMap.get(medicationName);
-            if (count > 0) {
-                medicationCountMap.put(medicationName, count - 1);
-            }
-        }
-    }
+	private HashMap<String, Integer> medications; // medication name -> number to take per day
+	
+	public void Meds() {
+	    medications = new HashMap<>();
+	        
+	}
+	
+	public void addMedication(String name, int numPerDay) {
+	    medications.put(name, numPerDay);
+	}
+	
+	public void promptMedicationSurvey() {
+	    // Prompt user with survey/questionnaire
+	    // Save information in medications HashMap
+	    // Assume input validation has been done outside of this method
+	    
+	    System.out.println("Do you have any medications to take?");
+	    String answer = System.console().readLine().toLowerCase();
+	    
+	    if (answer.equals("yes")) {
+	        System.out.println("How many for the week of 1/31?");
+	        int numTotal = Integer.parseInt(System.console().readLine());
+	        
+	        System.out.println("How many to take on Monday?");
+	        int numMonday = Integer.parseInt(System.console().readLine());
+	        
+	        System.out.println("How many to take on Tuesday?");
+	        int numTuesday = Integer.parseInt(System.console().readLine());
+	        
+	        System.out.println("How many to take on Wednesday?");
+	        int numWednesday = Integer.parseInt(System.console().readLine());
+	        
+	        System.out.println("How many to take on Thursday?");
+	        int numThursday = Integer.parseInt(System.console().readLine());
+	        
+	        System.out.println("How many to take on Friday?");
+	        int numFriday = Integer.parseInt(System.console().readLine());
+	        
+	        System.out.println("Do you have to take them on the weekend as well?");
+	        String weekendAnswer = System.console().readLine().toLowerCase();
+	        int numWeekend = 0;
+	        
+	        if (weekendAnswer.equals("yes")) {
+	            System.out.println("How many to take on Saturday?");
+	            int numSaturday = Integer.parseInt(System.console().readLine());
+	            
+	            System.out.println("How many to take on Sunday?");
+	            int numSunday = Integer.parseInt(System.console().readLine());
+	            
+	            numWeekend = numSaturday + numSunday;
+	        }
+	        
+	        int[] numPerDay = {numMonday, numTuesday, numWednesday, numThursday, numFriday, numWeekend, numWeekend};
+	        String[] daysOfWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+	        
+	        for (int i = 0; i < daysOfWeek.length; i++) {
+	            medications.put(daysOfWeek[i], numPerDay[i]);
+	        }
+	        
+	        int totalNumPerDay = numMonday + numTuesday + numWednesday + numThursday + numFriday + numWeekend + numWeekend;
+	        System.out.println("You need to take a total of " + totalNumPerDay + " medications per day.");
+	    }
+	}
+	
+    
 
     public void remindMedication(String medicationName) {
         // send notification or reminder to take the medication
         System.out.println("Reminder: Take " + medicationName);
     }
 
-    public void remindMedicationDay(String day) {
-        for (String medication : medicationList) {
-            int medicationCount = getMedicationCount(medication);
-            if (medicationCount > 0) {
-                // send notification or reminder to take the medication on this day
-                System.out.println("Reminder: Take " + medicationCount + " " + medication + " on " + day);
-            }
-        }
-    }
-}
+
+ }
+
 
--- Team1Iteration/src/Social.java
+++ Team1Iteration/src/Social.java
@@ -1,4 +1,38 @@
 
-public class Social {
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Social {
+    private List<String> socialClubs;
+
+    public Social() {
+        socialClubs = new ArrayList<>();
+    }
+
+    public void addSocialClub(String club) {
+        socialClubs.add(club);
+        setClubNotification(club);
+    }
+
+    public void setClubNotification(String club) {
+        // logic to set notification for social club
+        // notification we can set based on the meeting schedule of the club
+    	// or if user pre-adds dates they want to remember
+    }
+
+    public List<String> getSocialClubs() {
+        return socialClubs;
+    }
+
+    public void promptSocialClubInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input the social club you are part of: ");
+        String club = scanner.nextLine();
+        addSocialClub(club);
+        System.out.println("Social club added: " + club);
+    }
 }
+
+
